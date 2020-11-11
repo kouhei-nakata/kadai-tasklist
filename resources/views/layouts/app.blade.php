05046084029dc27e0c6ae0895587168866e8fbb4
kouhei <kouhe.news@gmail.com>
2020-11-11 12:45:06 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,13 @@
         @include('commons.navbar')
 
         <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_tasks')
+
             @yield('content')
         </div>
 
-        <!-- JavaScriptの指定は省略 -->
+        <!-- JavaScriptの定義は省略 -->
 
     </body>
 </html>
